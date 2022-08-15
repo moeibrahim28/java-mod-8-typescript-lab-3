ff6e41747e7d8b405ce75963623110c21de8127a
Moe <m.ibrahim0228@gmail.com>
2022-08-15 11:42:06 -0400

    input fields implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-// your code here
+
+function prepend() {
+  // your code here
+  let inputString: string = (<HTMLInputElement>document.getElementById("inputString")).value;
+  let numSpaces:number = parseInt((<HTMLInputElement>document.getElementById("numSpaces")).value);
+
+if(numSpaces!= null && inputString!= null){
+let spaces: string ="";
+  for(let x=0; x<numSpaces;x++){
+  spaces = spaces + "&nbsp;";
+  }
+   let result = spaces + inputString;
+   let output = "\"" + result + "\"";
+   console.log(output);
+   // Same as old Javascript
+   let outputElement = document.getElementById("output");
+   if (outputElement != null) {
+       outputElement.innerHTML = output;
+   }
+}
+else return "";
+
+}
+
